eba97e1299665f8381c8cecb2f81d62a76a381f6
davidcraig <davidcraig1986@gmail.com>
2025-11-13 17:12:11 +0000

    Remove console.log.

--- src/components/RenderCurrency.tsx
+++ src/components/RenderCurrency.tsx
@@ -1,5 +1,4 @@
 export function RenderCurrency(value: number) {
-  console.log(value);
   return (
     <span className={`currency ${value == 0 ? "zero" : "value"}`}>
       {value.toLocaleString("en-GB", { style: "currency", currency: "GBP" })}
